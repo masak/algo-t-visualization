3525f494e5ac8bc6746b4757d34b74cb661b1ada
Carl Mäsak <cmasak@gmail.com>
2019-06-27 22:43:17 +0200

    Add arrowheads
    They're straight; aiming to make them curved. But this is still
    an improvement.

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -27,6 +27,15 @@ function boxCoords(x: number, y: number): BoxCoords {
   return [200 * x - 100, 70 * y, 200 * x - 50, 70 * y + 50];
 }
 
+function angleOff(v: Vector, angleDegrees: number, distance: number): [number, number] {
+  let originalAngleRadians = Math.atan2(v.y2 - v.y1, v.x2 - v.x1);
+  let newAngleRadians = originalAngleRadians + angleDegrees * Math.PI / 180;
+  return [
+    v.x2 + Math.cos(newAngleRadians) * distance,
+    v.y2 + Math.sin(newAngleRadians) * distance,
+  ]
+}
+
 function Arrow({ fromX, fromY, toX, toY }: { fromX: number, fromY: number, toX: number, toY: number }) {
   let v: Vector = {
     x1: 200 * fromX - 75,
@@ -44,6 +53,8 @@ function Arrow({ fromX, fromY, toX, toY }: { fromX: number, fromY: number, toX: 
   }
   [v.x1, v.y1] = clipToBox(v, boxCoords(fromX, fromY));
   [v.x2, v.y2] = clipToBox(reverse(v), boxCoords(toX, toY));
+  let [x2h1, y2h1] = angleOff(v, 135, 15);
+  let [x2h2, y2h2] = angleOff(v, -135, 15);
   return (
     <>
       <line
@@ -53,6 +64,8 @@ function Arrow({ fromX, fromY, toX, toY }: { fromX: number, fromY: number, toX: 
         y2={v.y2}
         stroke="black"
       />
+      <path d={`M ${v.x2},${v.y2} L ${x2h1},${y2h1}`} stroke="black" />
+      <path d={`M ${v.x2},${v.y2} L ${x2h2},${y2h2}`} stroke="black" />
     </>
   );
 }
