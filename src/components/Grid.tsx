dc408a35f6cc5d59e8a98ec61378b915ced9374b
Carl Mäsak <cmasak@gmail.com>
2019-07-01 13:57:14 +0200

    Use `===` instead of `==`

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -3,10 +3,10 @@ import {gridSpacing} from '../lengths';
 
 function Grid({ width, height }: { width: number, height: number }) {
   let verticalLines = Array.from({ length: Math.floor(width / gridSpacing) }, (_, i) => (
-    <line key={i} x1={i * gridSpacing} y1="0" x2={i * gridSpacing} y2={height} strokeWidth={i % 4 == 0 ? 1 : 0.5} stroke="#333" />
+    <line key={i} x1={i * gridSpacing} y1="0" x2={i * gridSpacing} y2={height} strokeWidth={i % 4 === 0 ? 1 : 0.5} stroke="#333" />
   ));
   let horizontalLines = Array.from({ length: Math.floor(height / gridSpacing) }, (_, i) => (
-    <line key={i} x1="0" y1={i * gridSpacing} x2={width} y2={i * gridSpacing} strokeWidth={i % 4 == 0 ? 1 : 0.5} stroke="#333" />
+    <line key={i} x1="0" y1={i * gridSpacing} x2={width} y2={i * gridSpacing} strokeWidth={i % 4 === 0 ? 1 : 0.5} stroke="#333" />
   ));
   return (
     <>
